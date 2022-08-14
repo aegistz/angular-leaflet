700df3f52b7d522156790a61a9833526a1744ac2
teerayooti <teerayoot5056@gmail.com>
2022-08-14 16:56:47 +0700

    update
    import leaflet
    --> npm i leaflet --save-dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './ui/sidebar/sidebar.component';
+import { IndexComponent } from './pages/index/index.component';
+import { GetStartedComponent } from './pages/get-started/get-started.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    IndexComponent,
+    GetStartedComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+  public map: any
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(this.map);
+  }
+
+}
